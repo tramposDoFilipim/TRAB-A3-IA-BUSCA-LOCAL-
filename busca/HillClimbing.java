1a568d8ffef764f851c233990900e3fce7b39b39
Felipe <felipe.josue930@outlook.com>
2023-12-03 19:43:44 -0300

    projeto final

--- busca/HillClimbing.java
+++ busca/HillClimbing.java
@@ -4,36 +4,47 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class HillClimbing{
-    public int random(){
+    // instância um objeto para gerar um valor aleatorio
+    public int random()
+    {
         //O contrutor Random cria um novo gerador de números
         Random aleatorio = new Random();
         //O método nextInt(8) gera um número uniformemente localizado entre 0 e 7
         int random = aleatorio.nextInt(8);
         return random;
     }
-    public void gerarAleatoriamente(int[][] tabuleiro, int[] estado) {
-        //iteração através de índices de coluna
-        for (int i = 0; i < 8; i++) {
-            // Obtenção de um índice de linha aleatória
+    // Gera o quadro e posiciona a rainha aleatoriamente
+    public void gerarAleatoriamente(int[][] quadro, int[] estado)
+    {
+        //iteração através de índices de coluna.
+        for (int i = 0; i < 8; i++)
+        {
+            // Obtenção de um índice de linha aleatória.
             estado[i] = random();
 
-            // Coloca uma rainha na posição obtida no tabuleiro.
-            tabuleiro[estado[i]][i] = 1;
+            // Coloca uma rainha na posição obtida no quadro.
+            quadro[estado[i]][i] = 1;
         }
     }
-    // Uma função que imprime o "quadro" da matriz.
-    public void exibirTabuleiroDeXadrez(int[][] tabuleiro){
+    // Exibe o quadro
+    public void exibirTabuleiroDeXadrez(int[][] quadro)
+    {
 
-        for (int i = 0; i < 8; i++){
+        for (int i = 0; i < 8; i++)
+        {
             System.out.println("+-----+-----+-----+-----+-----+-----+-----+-----+");
-            for (int j = 0; j < 8; j++){
-                if (tabuleiro[i][j] == 1) {
+            for (int j = 0; j < 8; j++)
+            {
+                if (quadro[i][j] == 1)
+                {
                     System.out.print("|  Q  ");
                 }
-                if (tabuleiro[i][j] == 0) {
+                if (quadro[i][j] == 0)
+                {
                     System.out.print("|     ");
                 }
-                if (tabuleiro[i][j] == 7) {
+                if (quadro[i][j] == 7)
+                {
                     System.out.print("|");
                 }
             }
@@ -41,23 +52,24 @@ public class HillClimbing{
         }
         System.out.println("+-----+-----+-----+-----+-----+-----+-----+-----+");
     }
-    // Preenchendo a matriz "tabuleiro" com os valores "valor"
-    public void preencherTabuleiro(int[][] tabuleiro, int valor) {
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++){
-                tabuleiro[i][j] = valor;
+    // Preenchendo a matriz “quadro” com os valores “valor”
+    public void preencherTabuleiro(int[][] quadro, int valor)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                quadro[i][j] = valor;
             }
         }
     }
-    // Calcula o valor objetivo do Estado (as rainhas atacam entre elas)
-    // Função de avaliação heurística - o nome das regras que fazem atacar umas às outras.
-    public int hDoParDeAtaque(int[][] tabuleiro, int[] estado)
+    // vamos calcular o valor objetivo do estado (as senhoras atacam umas às outras)
+    // Função de avaliação heurística - o número de rainhas que atacam umas às outras.
+    public int hDoParDeAtaque(int[][] quadro, int[] estado)
     {
 
         /*
-        Para cada rainha em uma coluna, verificamos se outras rainhas
-        se alinham com a nossa rainha atual e se encontrarmos alguma,
-        qualquer, então incrementamos a contagem da variável de ataque.
+        Para cada rainha em uma coluna, verificamos se alguma outra rainha cai na linha de nossa rainha atual e, se encontrarmos alguma, aumentamos a contagem da variável de ataque.
         */
 
 
@@ -71,71 +83,78 @@ public class HillClimbing{
         int linha;
         int coluna;
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 8; i++)
+        {
         //Em cada coluna 'i', a rainha é colocada na linha 'estado[i]'
 
             //À esquerda da mesma linha
             linha = estado[i];
             coluna = i - 1;
-            while (coluna >= 0 && tabuleiro[linha][coluna] != 1){
+            while (coluna >= 0 && quadro[linha][coluna] != 1)
+            {
                 coluna--;
             }
-            if (coluna >= 0 && tabuleiro[linha][coluna] == 1)
+            if (coluna >= 0 && quadro[linha][coluna] == 1)
             {
-
                 atacando++;
-
             }
             // À direita da mesma linha
             linha = estado[i];
             coluna = i + 1;
-            while (coluna < 8 && tabuleiro[linha][coluna] != 1)
+            while (coluna < 8 && quadro[linha][coluna] != 1)
             {
                 coluna++;
             }
-            if (coluna < 8 && linha >= 0 && tabuleiro[linha][coluna] == 1)
+            if (coluna < 8 && linha >= 0 && quadro[linha][coluna] == 1)
             {
                 atacando++;
             }
             //Diagonal para Nordeste
             linha = estado[i] - 1;
             coluna = i + 1;
-            while (coluna < 8 && linha >= 0 && tabuleiro[linha][coluna] != 1) {
+            while (coluna < 8 && linha >= 0 && quadro[linha][coluna] != 1)
+            {
                 coluna++;
                 linha--;
             }
-            if (coluna < 8 && linha >= 0 && tabuleiro[linha][coluna] == 1) {
+            if (coluna < 8 && linha >= 0 && quadro[linha][coluna] == 1)
+            {
                 atacando++;
             }
             //Diagonal em direção Noroeste
             linha = estado[i] - 1;
             coluna = i - 1;
-            while (coluna >= 0 && linha >= 0 && tabuleiro[linha][coluna] != 1)
+            while (coluna >= 0 && linha >= 0 && quadro[linha][coluna] != 1)
             {
                 coluna--;
                 linha--;
             }
-            if (coluna >= 0 && linha >= 8 && tabuleiro[linha][coluna] == 1){
+            if (coluna >= 0 && linha >= 0 && quadro[linha][coluna] == 1)
+            {
                 atacando++;
             }
             //Diagonalmente em direção sudeste
             linha = estado[i] + 1;
             coluna = i + 1;
-            while (coluna < 8 && linha < 8 && tabuleiro[linha][coluna] != 1){
+            while (coluna < 8 && linha < 8 && quadro[linha][coluna] != 1)
+            {
                 coluna++;
                 linha++;
             }
-            if (coluna < 8 && linha < 8 && tabuleiro[linha][coluna] == 1){
+            if (coluna < 8 && linha < 8 && quadro[linha][coluna] == 1)
+            {
                 atacando++;
             }
             //Diagonalmente em direção ao sudoeste
             linha = estado[i] + 1;
             coluna = i - 1;
-            while (coluna >= 0 && linha < 8 && tabuleiro[linha][coluna] != 1){
+            while (coluna >= 0 && linha < 8 && quadro[linha][coluna] != 1)
+            {
                 coluna--;
                 linha++;
             }
-            if (coluna >= 0 && linha < 8 && tabuleiro[linha][coluna] == 1){
+            if (coluna >= 0 && linha < 8 && quadro[linha][coluna] == 1)
+            {
                 atacando++;
             }
         }
@@ -143,21 +162,24 @@ public class HillClimbing{
         return pares;
     }
     //gera uma configuração da matriz do tabuleiro com base no estado.
-    public void gerandoTabuleiro(int[][] tabuleiro, int[] estado){
+    public void gerandoTabuleiro(int[][] tabuleiro, int[] estado)
+    {
         preencherTabuleiro(tabuleiro, 0);
         for (int i = 0; i < 8; i++){
             tabuleiro[estado[i]][i] = 1;
         }
     }
     //copia o conteúdo do estado 2 para o estado 1
-    public void copia(int[] estado1, int[] estado2){
-        for (int i = 0; i < 8; i++){
+    public void copia(int[] estado1, int[] estado2)
+    {
+        for (int i = 0; i < 8; i++)
+        {
             estado1[i] = estado2[i];
         }
     }
     // Para obter o vizinho do estado atual com o menor valor objetivo entre todos os vizinhos, bem como o estado atual.
-    public void getVizinho(int[][] tabuleiro, int[] estado){
-
+    public void getVizinho(int[][] tabuleiro, int[] estado)
+    {
         // Declara e inicializa o tabuleiro e o estado ideal com o tabuleiro e o estado atual como ponto de partida.
         int[][] tabuleiroIdeal = new int[8][8];
         int[] estadoIdeal = new int[8];
@@ -176,7 +198,8 @@ public class HillClimbing{
         gerandoTabuleiro(quadroVizinho, estadoVizinho);
 
         // Iteração entre todos os possíveis vizinhos da matriz (Board).
-        for (int i = 0; i < 8; i++){
+        for (int i = 0; i < 8; i++)
+        {
             for (int j = 0; j < 8; j++) {
                 //Condição para pular o estado atual
                 if (j != estado[i]) {
@@ -214,13 +237,13 @@ public class HillClimbing{
         preencherTabuleiro(tabuleiro, 0);
         gerandoTabuleiro(tabuleiro, estado);
     }
-
-    public void hillClimbing(int[][] tabuleiro, int[] estado){
+    public void hillClimbing(int[][] tabuleiro, int[] estado)
+    {
 
         int[][] quadroVizinho = new int[8][8];
         int[] estadoVizinho = new int[8];
 
-        // inicializa a matriz e o estado vizinhos com a placa e o estado da matriz atual
+        // inicializa a matriz e o estado vizinhos com o tabuleiro e o estado da matriz atual
         copia(estadoVizinho, estado);
         gerandoTabuleiro(quadroVizinho, estadoVizinho);
 
@@ -246,8 +269,8 @@ public class HillClimbing{
             if (Arrays.equals(estado, estadoVizinho)) {
 
                 /*
-				 Se o estado vizinho e o estado real forem iguais,
-                 então não há vizinho ideal e, conseqüentemente,
+				 Se o estado vizinho e o estado atual forem iguais,
+                 então não há vizinho ideal e, consequentemente,
                  o resultado é exibido
 				 */
 
@@ -256,8 +279,9 @@ public class HillClimbing{
                 // Muda Continue de verdadeiro para falso para sair do loop
                 // O loop será interrompido
                 Continue = false;
-            } else if (hDoParDeAtaque(tabuleiro, estado) == hDoParDeAtaque(quadroVizinho, estadoVizinho)) {
-
+            }
+            else if (hDoParDeAtaque(tabuleiro, estado) == hDoParDeAtaque(quadroVizinho, estadoVizinho))
+            {
                 estadoVizinho[random()] = random();
                 gerandoTabuleiro(quadroVizinho, estadoVizinho);
 
